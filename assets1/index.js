9822a229d2cea5e30e7b3d98b4227fe38b931a13
Inna Danylchuk <innadanylchuk13@gmail.com>
2024-05-09 19:03:31 +0200

    First commit

---
+++ assets1/index.js
@@ -0,0 +1,44 @@
+
+const a = '-3';
+const b = 5;
+const ab = a*b;
+console.log(ab);
+
+const c = 10;
+const d = 0;
+const cd = c/d;
+console.log(cd);
+
+const e = 'Слава';
+const f = ' Україні';
+const ef = e+f;
+console.log(ef);
+
+const eleven = 11;
+const isTrue = true;
+const js = 'java script';
+const string = '100';
+console.log(eleven,isTrue,js,string);
+
+/*num+=11;
+num*=11;
+num-=11;
+ num/=11;
+ num++;
+ num--;*/
+
+/*let variable0 = Number(prompt('Please input a number'));
+alert(variable1**2)*/
+
+/*let variable1 = Number(prompt('PLease input first number:'))
+let variable2 = Number(prompt('PLease input second number:'));
+let variable3 = (variable1+variable2)/2;
+console.log(variable3);*/
+
+/*let hvylyny = Number(prompt('Input minuts '))
+let sekundy = hvylyny*60
+alert(sekundy)*/
+
+/*let greeting = 'Hello,';
+let userName  = prompt('PLease provide your name')
+document.write(`<p> ${greeting} ${userName} !</p>`)*/
